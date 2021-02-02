bfb7dd537ff8d61b722e7f0759799ce0c909deed
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2021-02-02 16:28:21 -0500

    Update s2ef_mapping.py

--- experimental/s2ef_mapping.py
+++ experimental/s2ef_mapping.py
@@ -5,13 +5,13 @@ old_systems = glob.glob("path_to_old_systems/*.traj")
 new_system_path = "path_to_new_systems"
 
 system_mappings = {}
-for system in old_system:
+for system in old_systems:
     randomid = os.path.basename(system)[:-5]
     old_images = ase.io.read(system, ":")
-    old_energies = [image.get_potential_energy() for image in old_iamges]
+    old_energies = [image.get_potential_energy() for image in old_images]
 
     new_images = ase.io.read(f"{new_system_path}/{randomid}.traj", ":")
-    new_energies = [image.get_potential_energy() for image in new_iamges]
+    new_energies = [image.get_potential_energy() for image in new_images]
 
     dict_mapping = {}
     for old_idx, energy in enumerate(old_energies):
