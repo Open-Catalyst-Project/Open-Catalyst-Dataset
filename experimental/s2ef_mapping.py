0a99e33a58f22faae2a401499a0800aed1e64b19
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2021-02-03 18:59:17 +0000

    positions instead of energies

--- experimental/s2ef_mapping.py
+++ experimental/s2ef_mapping.py
@@ -9,26 +9,27 @@ import numpy as np
 
 def get_mapping(system):
     randomid = os.path.basename(system)[:-7]
-    old_images = ase.io.read(system, ":")
-    old_energies = [image.get_potential_energy() for image in old_images]
+    try:
+        old_images = ase.io.read(system, ":")
+        old_pos = [image.get_positions() for image in old_images]
 
-    new_images = ase.io.read(f"/home/jovyan/projects/ocp/data/trajs/train_02_01/{randomid}.traj", ":")
-    new_energies = [image.get_potential_energy() for image in new_images]
+        new_images = ase.io.read(f"/home/jovyan/projects/ocp/data/trajs/train_02_01/{randomid}.traj", ":")
+        new_pos = [image.get_positions() for image in new_images]
 
-    dict_mapping = {}
-    # maps old frame idx -> new frame idx
-    for old_idx, energy in enumerate(old_energies):
-        new_idx = np.where(new_energies == energy)[0]
-        try:
-            assert len(new_idx) == 1 # ensure no duplicates
-        except:
-            return (randomid, False)
+        dict_mapping = {}
+        # maps old frame idx -> new frame idx
+        for old_idx, pos in enumerate(old_pos):
+            for new_idx, pos_2 in enumerate(new_pos):
+                if (pos == pos_2).all():
+                    dict_mapping[old_idx] = new_idx
+                    break
+    except:
+        return (randomid, False)
 
-        dict_mapping[old_idx] = new_idx.item()
     return (randomid, dict_mapping)
 
 if __name__ == "__main__":
-    old_systems = glob.glob("/home/jovyan/projects/ocp/data/trajs/train/*.extxyz")[:10]
+    old_systems = glob.glob("/home/jovyan/projects/ocp/data/trajs/train/*.extxyz")
 
     pool = mp.Pool(30)
     results = list(tqdm(pool.imap(get_mapping, old_systems), total=len(old_systems)))
@@ -42,4 +43,9 @@ if __name__ == "__main__":
         else:
             issues.append(system)
 
-    # write issues and system_mappings to disk
+    with open("s2ef_mapping_issues_pos.txt", "a") as f:
+        for system in issues:
+            f.write(f"{system}\n")
+
+    with open("s2ef_mappings_pos.pkl", "wb") as f:
+        pickle.dump(system_mappings, f)
