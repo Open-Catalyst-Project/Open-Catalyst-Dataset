8da2dd0000c749e1773ad7d1ef3fa78734407412
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2021-02-04 02:35:46 +0000

    latest s2ef mapping

--- experimental/s2ef_mapping.py
+++ experimental/s2ef_mapping.py
@@ -1,19 +1,41 @@
-from tqdm import tqdm
+import sys
 import glob
 import ase.io
 import pickle
 import os
 import multiprocessing as mp
+from tqdm import tqdm
 import numpy as np
 
 
-def get_mapping(system):
-    randomid = os.path.basename(system)[:-7]
+def read_old(randomid):
+    dirs = [
+        "trajectories", "trajectories_phase2", "trajectories_val_test"
+    ]
+    for folder in dirs:
+        path = "/global/cfs/cdirs/m2755/OC20_dataset/{}/{}.traj".format(
+            folder, randomid
+        )
+        if os.path.isfile(path):
+            return ase.io.read(path, ":")
+
+def read_new(randomid):
+    dirs = [
+        "train", "test", "val"
+    ]
+    for folder in dirs:
+        path = "/global/cfs/cdirs/m2755/OC20_dataset/new_trajs_02_03/{}/{}.traj".format(
+            folder, randomid
+        )
+        if os.path.isfile(path):
+            return ase.io.read(path, ":")
+
+def get_mapping(randomid):
     try:
-        old_images = ase.io.read(system, ":")
+        old_images = read_old(randomid)
         old_pos = [image.get_positions() for image in old_images]
 
-        new_images = ase.io.read(f"/home/jovyan/projects/ocp/data/trajs/train_02_01/{randomid}.traj", ":")
+        new_images = read_new(randomid)
         new_pos = [image.get_positions() for image in new_images]
 
         dict_mapping = {}
@@ -23,16 +45,26 @@ def get_mapping(system):
                 if (pos == pos_2).all():
                     dict_mapping[old_idx] = new_idx
                     break
-    except:
+        assert len(set(dict_mapping.keys())) == len(set(dict_mapping.values()))
+    except Exception:
         return (randomid, False)
 
     return (randomid, dict_mapping)
 
-if __name__ == "__main__":
-    old_systems = glob.glob("/home/jovyan/projects/ocp/data/trajs/train/*.extxyz")
 
-    pool = mp.Pool(30)
-    results = list(tqdm(pool.imap(get_mapping, old_systems), total=len(old_systems)))
+if __name__ == "__main__":
+    all_frames = open(
+        "/global/cfs/cdirs/m2755/OC20_dataset/splits/ef_frames.txt", "r"
+    ).read().splitlines()
+
+    chunk = int(sys.argv[1])
+    slurm_chunks = np.array_split(all_frames, 20)
+    ef_frames = slurm_chunks[chunk]
+
+    pool = mp.Pool(mp.cpu_count())
+    results = list(
+        tqdm(pool.imap(get_mapping, ef_frames), total=len(ef_frames))
+    )
 
     issues = []
     system_mappings = {}
@@ -43,9 +75,9 @@ if __name__ == "__main__":
         else:
             issues.append(system)
 
-    with open("s2ef_mapping_issues_pos.txt", "a") as f:
+    with open(f"ef_mappings/issues_ef_mapping_chunk_{chunk}.txt", "a") as f:
         for system in issues:
             f.write(f"{system}\n")
 
-    with open("s2ef_mappings_pos.pkl", "wb") as f:
+    with open(f"ef_mappings/ef_mappings_chunk_{chunk}.pkl", "wb") as f:
         pickle.dump(system_mappings, f)
