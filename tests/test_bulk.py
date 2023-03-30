7887c1d9cc60e756afbfe67c9d17073198f1ea6c
Muhammed Shuaibi <mushuaibi@gmail.com>
2023-03-30 13:34:28 -0700

    ci directory structure fix

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -10,7 +10,10 @@ from ocdata.core import Bulk
 
 @pytest.fixture(scope="class")
 def load_bulk(request):
+    cwd = os.getcwd()
     request.cls.idx = 0
+
+    request.cls.precomputed_path = os.path.join(cwd, str(request.cls.idx) + ".pkl")
     request.cls.bulk = Bulk(bulk_id_from_db=request.cls.idx)
 
 
@@ -29,7 +32,6 @@ class TestBulk:
 
     def test_unique_slab_enumeration(self):
         slabs = self.bulk.compute_slabs()
-        cwd = os.getcwd()
 
         seen = []
         for slab in slabs:
@@ -39,11 +41,11 @@ class TestBulk:
         # pymatgen-2023.1.20 + ase 3.22.1
         assert len(slabs) == 15
 
-        with open(f"{os.path.join(cwd, str(self.idx))}.pkl", "wb") as f:
+        with open(self.precomputed_path, "wb") as f:
             pickle.dump(slabs, f)
 
     def test_precomputed_slab(self):
-        self.bulk.precomputed_slabs_path = "."
+        self.bulk.precomputed_slabs_path = os.path.dirname(self.precomputed_path)
 
         precomputed_slabs = self.bulk.get_precomputed_slabs()
         assert len(precomputed_slabs) == 15
@@ -51,7 +53,7 @@ class TestBulk:
         slabs = self.bulk.compute_slabs()
         assert precomputed_slabs[0] == slabs[0]
 
-        os.remove(f"{self.idx}.pkl")
+        os.remove(self.precomputed_path)
 
     def test_slab_miller_enumeration(self):
         slabs_max_miller_1 = self.bulk.compute_slabs(max_miller=1)
