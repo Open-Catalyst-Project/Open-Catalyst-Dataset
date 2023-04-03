3f21745ed649e46eab83a8359801e96b7ab79cf5
Abhishek Das <das.abhshk@gmail.com>
2023-04-03 11:47:00 -0700

    Updates test_inputs to latest api

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -2,22 +2,26 @@ import random
 
 import pytest
 
-from ocdata.core import Adslab, Adsorbate, Bulk, Surface
+from ocdata.core import Adsorbate, AdsorbateSlabConfig, Bulk, Slab
 from ocdata.utils.vasp import VASP_FLAGS, _clean_up_inputs
 
 
 @pytest.fixture(scope="class")
 def load_data(request):
     bulk_sample_1 = Bulk(bulk_id_from_db=24)
-    surface_sample_1 = Surface(bulk_sample_1)
+    slab_sample_1 = Slab.from_bulk_get_random_slab(bulk_sample_1)
     adsorbate_sample_1 = Adsorbate(adsorbate_id_from_db=10)
 
     bulk_sample_2 = Bulk(bulk_id_from_db=100)
-    surface_sample_2 = Surface(bulk_sample_2)
+    slab_sample_2 = Slab.from_bulk_get_random_slab(bulk_sample_2)
     adsorbate_sample_2 = Adsorbate(adsorbate_id_from_db=2)
 
-    request.cls.adslab1 = Adslab(surface_sample_1, adsorbate_sample_1, num_sites=100)
-    request.cls.adslab2 = Adslab(surface_sample_2, adsorbate_sample_2, num_sites=100)
+    request.cls.adslab1 = AdsorbateSlabConfig(
+        slab_sample_1, adsorbate_sample_1, num_sites=100
+    )
+    request.cls.adslab2 = AdsorbateSlabConfig(
+        slab_sample_2, adsorbate_sample_2, num_sites=100
+    )
 
     ALT_VASP_FLAGS = VASP_FLAGS.copy()
     ALT_VASP_FLAGS["nsw"] = 0
@@ -29,7 +33,7 @@ def load_data(request):
 @pytest.mark.usefixtures("load_data")
 class TestVasp:
     def test_cleanup(self):
-        atoms = self.adslab1.structures[0][1]
+        atoms = self.adslab1.atoms_list[0]
         atoms1, flags1 = _clean_up_inputs(atoms, VASP_FLAGS)
 
         # Check that kpts are computed and added to the flags
@@ -43,8 +47,8 @@ class TestVasp:
         assert flags2 != flags1
 
     def test_unique_kpts(self):
-        atoms1 = self.adslab1.structures[0][1]
-        atoms2 = self.adslab2.structures[0][1]
+        atoms1 = self.adslab1.atoms_list[0]
+        atoms2 = self.adslab2.atoms_list[0]
 
         _, flags1 = _clean_up_inputs(atoms1, VASP_FLAGS)
         _, flags2 = _clean_up_inputs(atoms2, VASP_FLAGS)
