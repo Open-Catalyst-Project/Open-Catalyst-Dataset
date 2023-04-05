ac78be51e6108b9d69c071386844617d715ff4ab
Abhishek Das <das.abhshk@gmail.com>
2023-04-05 07:39:36 -0700

    Remove unused functions from vasp utils

--- ocdata/utils/vasp.py
+++ ocdata/utils/vasp.py
@@ -52,27 +52,6 @@ BULK_VASP_FLAGS = {
 }
 
 
-def run_vasp(atoms, vasp_flags=None):
-    """
-    Will relax the input atoms given the VASP flag inputs.
-
-    Args:
-        atoms       `ase.Atoms` object that we want to relax.
-        vasp_flags  A dictionary of settings we want to pass to the `Vasp`
-                    calculator. Defaults to a standerd set of values if `None`
-    Returns:
-        trajectory  A list of `ase.Atoms` objects where each element represents
-        each step during the relaxation.
-    """
-    if vasp_flags is None:  # Immutable default
-        vasp_flags = VASP_FLAGS.copy()
-
-    atoms, vasp_flags = _clean_up_inputs(atoms, vasp_flags)
-    vasp_flags = _set_vasp_command(vasp_flags)
-    trajectory = relax_atoms(atoms, vasp_flags)
-    return trajectory
-
-
 def _clean_up_inputs(atoms, vasp_flags):
     """
     Parses the inputs and makes sure some things are straightened out.
@@ -125,59 +104,6 @@ def calculate_surface_k_points(atoms):
     return k_pts
 
 
-def _set_vasp_command(n_processors=16, vasp_executable="vasp_std"):
-    """
-    This function assigns the appropriate call to VASP to the `$VASP_COMMAND`
-    variable.
-    """
-    # TODO:  Sid and/or Caleb to figure out what exactly to put here to make
-    # things work. Here are some examples:
-    # https://github.com/ulissigroup/GASpy/blob/master/gaspy/vasp_functions.py#L167
-    # https://github.com/ulissigroup/GASpy/blob/master/gaspy/vasp_functions.py#L200
-    command = "srun -n %d %s" % (n_processors, vasp_executable)
-    os.environ["VASP_COMMAND"] = command
-    raise NotImplementedError
-
-
-def relax_atoms(atoms, vasp_flags):
-    """
-    Perform a DFT relaxation with VASP and then write the trajectory to the
-    'relaxation.traj' file.
-
-    Args:
-        atoms       `ase.Atoms` object of the structure we want to relax
-        vasp_flags  A dictionary of settings we want to pass to the `Vasp`
-                    calculator
-    Returns:
-        images      A list of `ase.Atoms` that comprise the relaxation
-                    trajectory
-    """
-    # Run the calculation
-    calc = Vasp(**vasp_flags)
-    atoms.set_calculator(calc)
-    atoms.get_potential_energy()
-
-    # Read the trajectory from the output file
-    images = []
-    for atoms in ase.io.read("vasprun.xml", ":"):
-        image = atoms.copy()
-        image = image[calc.resort]
-        image.set_calculator(
-            SPC(
-                image,
-                energy=atoms.get_potential_energy(),
-                forces=atoms.get_forces()[calc.resort],
-            )
-        )
-        images += [image]
-
-    # Write the trajectory
-    with TrajectoryWriter("relaxation.traj", "a") as writer:
-        for atoms in images:
-            writer.write(atoms)
-    return images
-
-
 def write_vasp_input_files(atoms, outdir=".", vasp_flags=None):
     """
     Effectively goes through the same motions as the `run_vasp` function,
@@ -196,49 +122,3 @@ def write_vasp_input_files(atoms, outdir=".", vasp_flags=None):
     atoms, vasp_flags = _clean_up_inputs(atoms, vasp_flags)
     calc = Vasp(directory=outdir, **vasp_flags)
     calc.write_input(atoms)
-
-
-def xml_to_tuples(xml="vasprun.xml"):
-    """
-    Converts an XML file into both a trajectory file while also returning the
-    trajectory as a list of `ase.Atoms` objects
-
-    Args:
-        xml     String indicating the XML file to read from
-    Returns:
-        images  A list of 5-tuples for each images in the trajectory. The
-                tuples include a list of symbols for each atom; the positions
-                of the atoms; the forces each atom sees; the unit cell
-                dimensions; and the potential energy of the whole system.
-    """
-    traj = xml_to_traj(xml)
-
-    images = []
-    for atoms in traj:
-        symbols = atoms.get_chemical_symbols()
-        positions = atoms.get_positions()
-        forces = atoms.get_forces()
-        cell = np.array(atoms.get_cell())
-        energy = atoms.get_potential_energy()
-        atoms_tuple = (symbols, positions, forces, cell, energy)
-        images.append(atoms_tuple)
-
-    return images
-
-
-def xml_to_traj(xml="vasprun.xml"):
-    """
-    Converts an XML file into both a trajectory file while also returning the
-    trajectory as a list of `ase.Atoms` objects
-
-    Args:
-        xml     String indicating the XML file to read from
-    Returns:
-        traj    A list of `ase.Atoms` objects
-    """
-    traj = ase.io.read(xml, ":")
-    for atoms in traj:
-        atoms.set_calculator(
-            SPC(atoms, energy=atoms.get_potential_energy(), forces=atoms.get_forces())
-        )
-    return traj
